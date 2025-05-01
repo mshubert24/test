12ddf716fb613dcf2c4659323cc3c094f7e7c24b
Matthew Shubert <matthewshubert15@gmail.com>
2025-05-01 14:14:59 -0400

    Remove Hello World

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ Practice file to familiarize myself with git commands
 Author: Matthew Shuber
 """
 
-print("Hello World")
+
 
 print("This change is specific to the variant_ms")
 print("This change is my 2nd change to the variant_ms")
